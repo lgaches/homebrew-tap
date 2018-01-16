57dd7f194a081d02364edc83664e19eb75c9677b
Laurent Gaches <laurent@binimo.com>
2018-01-16 16:33:48 +0100

    fix case

--- CMosquitto.rb
+++ CMosquitto.rb
@@ -1,7 +1,7 @@
 $version = "0.0.2"
 $hash = "c2e723da90d3f81caff2ebc00584fc81cd50ed3be350ae673e7917e4173b9c28"
 
-class CMosquitto < Formula
+class Cmosquitto < Formula
   homepage "https://github.com/lgaches/CMosquitto"
   url "https://github.com/lgaches/CMosquitto/archive/#{$version}.tar.gz"
   sha256 $hash
